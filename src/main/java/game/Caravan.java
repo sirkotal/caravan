e855f60e2eea55fde93b963f7840bbabd3eb52d9
sirkotal <lordkotal2002@gmail.com>
2025-03-20 12:23:50 +0000

    Working on adding cards to a caravan

--- src/main/java/game/Caravan.java
+++ src/main/java/game/Caravan.java
@@ -24,13 +24,45 @@ public class Caravan {
     public void setSuit(String suit) {
         this.suit = suit;
     }
-    
+
     public void setAscending(boolean bool) {
         this.ascending = bool;
     }
 
-    public void addCard(Card card) {
-        this.caravan.add(card);
-        this.value += card.getValue();
+    public boolean addCard(Card card, int... pos) {
+        Card lastCard = this.caravan.getLast();
+        if (!card.isFace() || Objects.equals(card.getID(), "ace")) {
+            if ((card.getValue() > lastCard.getValue() && ascending) || (card.getValue() < lastCard.getValue() && !ascending)) {
+                this.caravan.add(card);
+                this.value += card.getValue();
+                setSuit(card.getSuit());
+                return true;
+            }
+            else if (Objects.equals(card.getSuit(), lastCard.getSuit())) {
+                this.caravan.add(card);
+                this.value += card.getValue();
+                setAscending((card.getValue() > lastCard.getValue()));
+                return true;
+            }
+        }
+        else {
+            switch (card.getID()) {
+                case "queen":
+                    this.caravan.add(card);
+                    setSuit(card.getSuit());
+                    setAscending(!this.ascending);
+                    return true;
+                case "jack":
+                    //TODO
+                case "king":
+                    this.caravan.add(pos[0], card);
+                    this.value += this.caravan.get(pos[0] - 1).getValue();
+                    setSuit(card.getSuit());
+                    return true;
+                case "joker":
+                    //TODO
+            }
+        }
+        return false;
     }
 }
